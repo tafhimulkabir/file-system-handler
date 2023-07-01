abf3cd757a278a8963d3bd556f9b74db2e09cd4d
Tafhimul kabir <tafhimulkabir@protonmail.com>
2023-07-01 23:22:03 +0600

    Updated FileSystemHandler class

--- FileSystemHandler.php
+++ FileSystemHandler.php
@@ -2,9 +2,10 @@
 /**
  * File System Handler
  *
- * This PHP class fetch files and folders recursively
+ * This PHP class fetches files and folders recursively.
  */
-declare(strict_types = 1);
+
+declare(strict_types=1);
 
 namespace App\Utility;
 
@@ -15,22 +16,53 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'helper.php';
 
 class FileSystemHandler
 {
-    private const BASE_PATH = __DIR__ . DIRECTORY_SEPARATOR;
     private string $dirPath;
 
-    public function fetch(string $path, string $type = 'both', bool $recursion = false): array
-    {
-        $this->validatePath($path);
-        return [];
-    }
-
-    private function validatePath(string $path): string
+    public function fetch(string $path, string $type = 'both', bool $recursion = false): void
     {
         $path = realpath(trim($path));
-        if ($path !== false && is_dir($path) && is_file_path($path)) {
-            return $path;
-        } else {
-            throw new \InvalidArgumentException('Invalid Path : ' . $path);
+
+        $this->validatePath($path);
+
+        $this->process($path, $type, $recursion);
+    }
+
+    private function validatePath(string $path): void
+    {
+        if ($path === false) {
+            throw new \InvalidArgumentException('No directory found at: ' . $path);
+        }
+        if (!is_dir($path)) {
+            throw new \InvalidArgumentException('Invalid directory: ' . $path);
+        }
+        if (!is_file_path($path)) {
+            throw new \InvalidArgumentException('Invalid path: ' . $path);
+        }
+    }
+
+    private function process(string $path, string $type, bool $recursion): void
+    {
+        $iterator = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS),
+            RecursiveIteratorIterator::SELF_FIRST
+        );
+
+        foreach ($iterator as $item) {
+            $filename = $item->getFilename();
+            $filepath = $item->getPathname();
+
+            if ($type === 'both' || ($type === 'dir' && $item->isDir()) || ($type === 'file' && $item->isFile())) {
+                echo 'Filename: ' . $filename . PHP_EOL;
+                echo 'Filepath: ' . $filepath . PHP_EOL;
+                echo '--------------------------' . PHP_EOL;
+            }
+
+            if ($recursion === false && $item->isDir()) {
+                $iterator->setMaxDepth(0);
+            }
         }
     }
 }
+
+$file = new FileSystemHandler();
+$file->fetch('..');
